0534beaadbef337e2ac7e658d70370db016f761d
Spencer Carli <spencercarli@gmail.com>
2019-08-20 09:52:56 -0500

    Link firestore

--- android/app/src/main/java/com/handlebarlbas/chatdemo/MainApplication.java
+++ android/app/src/main/java/com/handlebarlbas/chatdemo/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 
 import java.util.List;
 
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RNFirebaseFirestorePackage());
       return packages;
     }
 
