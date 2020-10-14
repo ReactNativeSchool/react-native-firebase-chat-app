ef8e4fa337f39ce78554e8e1451af81cced055b5
Spencer Carli <spencercarli@gmail.com>
2020-10-14 11:16:56 -0500

    Remove code fro 'Configure Gifted Chat'

--- App/screens/Messages.js
+++ App/screens/Messages.js
@@ -1,21 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
 
 export default () => {
-  const [messages, setMessages] = useState([]);
-
-  return (
-    <View style={{ backgroundColor: '#fff', flex: 1 }}>
-      <GiftedChat
-        messages={messages}
-        onSend={(newMessages) => {
-          setMessages(GiftedChat.append(messages, newMessages));
-        }}
-        user={{
-          _id: 1,
-        }}
-      />
-    </View>
-  );
+  return <View style={{ backgroundColor: '#fff', flex: 1 }} />;
 };
