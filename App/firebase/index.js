cf323f5b33cdb0ec51a16fe2d16b42b3875a0fae
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-04-08 11:25:53 -0500

    Determining If a Thread has Unread Messages

--- App/firebase/index.js
+++ App/firebase/index.js
@@ -91,3 +91,8 @@ export const markThreadLastRead = threadId => {
       {merge: true},
     );
 };
+
+export const listenToThreadTracking = () => {
+  const user = currentUser();
+  return firestore().collection('USER_THREAD_TRACK').doc(user.uid);
+};
--- App/screens/Threads.js
+++ App/screens/Threads.js
@@ -2,10 +2,27 @@ import React, {useState, useEffect} from 'react';
 import {FlatList, View} from 'react-native';
 
 import {ThreadRow, Separator} from '../components/ThreadRow';
-import {listenToThreads} from '../firebase';
+import {listenToThreads, listenToThreadTracking} from '../firebase';
+
+const isThreadUnread = (thread, threadTracking) => {
+  console.log(
+    threadTracking[thread._id] < thread.latestMessage.createdAt,
+    thread.latestMessage.createdAt,
+    threadTracking[thread._id],
+  );
+  if (
+    threadTracking[thread._id] &&
+    threadTracking[thread._id] < thread.latestMessage.createdAt
+  ) {
+    return true;
+  }
+
+  return false;
+};
 
 export default ({navigation}) => {
   const [threads, setThreads] = useState([]);
+  const [threadTracking, setThreadTracking] = useState({});
 
   useEffect(() => {
     const unsubscribe = listenToThreads().onSnapshot(querySnapshot => {
@@ -27,6 +44,16 @@ export default ({navigation}) => {
     };
   }, []);
 
+  useEffect(() => {
+    const unsubscribe = listenToThreadTracking().onSnapshot(snapshot => {
+      setThreadTracking(snapshot.data() || {});
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <View style={{flex: 1, backgroundColor: '#fff', paddingBottom: 50}}>
       <FlatList
@@ -36,6 +63,7 @@ export default ({navigation}) => {
           <ThreadRow
             {...item}
             onPress={() => navigation.navigate('Messages', {thread: item})}
+            unread={isThreadUnread(item, threadTracking)}
           />
         )}
         ItemSeparatorComponent={() => <Separator />}
