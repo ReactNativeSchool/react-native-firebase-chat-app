a47cb79bd7368302d7f07c22144fc49821c00d19
Spencer Carli <1105429+spencercarli@users.noreply.github.com>
2022-04-08 10:47:19 -0500

    Anonymous Firebase Authentication

--- App/screens/Initializing.js
+++ App/screens/Initializing.js
@@ -1,1 +1,36 @@
-export default () => null;
+import {useEffect} from 'react';
+import auth from '@react-native-firebase/auth';
+import {
+  uniqueNamesGenerator,
+  adjectives,
+  colors,
+  animals,
+} from 'unique-names-generator';
+
+export default ({onHasUser}) => {
+  useEffect(() => {
+    const unsubscribe = auth().onAuthStateChanged(authUser => {
+      if (authUser) {
+        console.log(authUser);
+        return onHasUser();
+      }
+
+      return auth()
+        .signInAnonymously()
+        .then(({user}) => {
+          return user.updateProfile({
+            displayName: uniqueNamesGenerator({
+              dictionaries: [adjectives, colors, animals],
+            }),
+          });
+        })
+        .then(() => {
+          onHasUser();
+        });
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return null;
+};
